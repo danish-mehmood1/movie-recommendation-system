44580309a5fa2f68ad4c1d29992e1ed9e27b0b04
Danish Mehmood <danish.mehmood1436@gmail.com>
2026-07-17 22:53:37 +0500

    Add Streamlit secrets support

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
+def get_secret(key):
+    try:
+        return st.secrets[key]
+    except:
+        return os.getenv(key)
+
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT")),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME")
+    "host": get_secret("DB_HOST"),
+    "port": int(get_secret("DB_PORT")),
+    "user": get_secret("DB_USER"),
+    "password": get_secret("DB_PASSWORD"),
+    "database": get_secret("DB_NAME")
 }
 
-TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_API_KEY = get_secret("TMDB_API_KEY")
 
 APP_NAME = "Movie Recommendation System"
 APP_VERSION = "1.0.0"
